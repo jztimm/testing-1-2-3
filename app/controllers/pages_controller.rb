80bd9cede4811a643065b4ce9b1b869e87dbb05c
jztimm <jztimm@gmail.com>
2020-08-13 11:01:31 -0400

    setup initial routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+# Controls all of the code for any page inside of /pages
+class PagesController < ApplicationController
+  
+  def index
+  end
+
+  def home
+  end
+
+  def profile
+  end
+
+  def explore
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # Won't work because of custom routes
+  # resources :pages, only: [:index, :home, :profile, :explore]
+  
+  root 'pages#index'
+
+  get 'home', to: 'pages#home'
+  get 'profile', to: 'pages#profile'
+  get 'explore', to: 'pages#explore'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
